20b81a6f4d90884f2568800e9889946dff573d28
Sherry Freitas <sherry.freitas@gmail.com>
2019-06-21 12:52:40 -0700

    adds initial files

---
+++ server/datasources.local.js
@@ -0,0 +1,29 @@
+module.exports = {
+  'db': {
+    'name': 'db',
+    'connector': 'memory',
+  },
+  'MongoDB': {
+    'url': 'mongodb://localhost:27017',
+    'name': 'MongoDB',
+    'connector': 'mongodb',
+  },
+};
+
+// module.exports = {
+//   "db": {
+//     "name": "db",
+//     "connector": "memory"
+//   },
+//   "MongoDB": {
+//     "host": "",
+//     "port": 0,
+//     "url": "",
+//     "database": "",
+//     "password": "",
+//     "name": "MongoDB",
+//     "user": "",
+//     "useNewUrlParser": true,
+//     "connector": "mongodb"
+//   }
+// }
--- test/server.spec.js
+++ test/server.spec.js
@@ -39,5 +39,4 @@ describe('loopback server', function() {
         done();
       });
   });
-
-})
+});
