37ba45eaeb41f6421cd8f355bf91a68ded4b1e2e
Sherry Freitas <sherry.freitas@gmail.com>
2019-06-21 13:27:32 -0700

    fixes datasources

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -1,29 +1,29 @@
-// module.exports = {
-//   'db': {
-//     'name': 'db',
-//     'connector': 'memory',
-//   },
-//   'MongoDB': {
-//     'url': 'mongodb://localhost:27017',
-//     'name': 'MongoDB',
-//     'connector': 'mongodb',
-//   },
-// };
-
 module.exports = {
-  "db": {
-    "name": "db",
-    "connector": "memory"
+  'db': {
+    'name': 'db',
+    'connector': 'memory',
   },
-  "MongoDB": {
-    "host": "",
-    "port": 0,
-    "url": "",
-    "database": "",
-    "password": "",
-    "name": "MongoDB",
-    "user": "",
-    "useNewUrlParser": true,
-    "connector": "mongodb"
-  }
-}
+  'MongoDB': {
+    'url': 'mongodb://heroku_30bsb9xl:n9hib6tk1cv423e15ss8ueg6on@ds241537.mlab.com:41537/heroku_30bsb9xl',
+    'name': 'MongoDB',
+    'connector': 'mongodb',
+  },
+};
+
+// module.exports = {
+//   "db": {
+//     "name": "db",
+//     "connector": "memory"
+//   },
+//   "MongoDB": {
+//     "host": "",
+//     "port": 0,
+//     "url": "",
+//     "database": "",
+//     "password": "",
+//     "name": "MongoDB",
+//     "user": "",
+//     "useNewUrlParser": true,
+//     "connector": "mongodb"
+//   }
+// }
